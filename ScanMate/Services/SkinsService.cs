9b33bd7441dec32456d6d17e52acc4319c56e9a2
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-04-02 13:06:22 +0200

    enable & desable add button

--- ScanMate/Services/SkinsService.cs
+++ ScanMate/Services/SkinsService.cs
@@ -15,5 +15,6 @@ public class SkinsService : ContentPage
 
         return skinsList;
     }
+
 }
 
--- ScanMate/ViewModel/SkinsViewModel.cs
+++ ScanMate/ViewModel/SkinsViewModel.cs
@@ -1,9 +1,23 @@
+using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
+
 namespace ScanMate.ViewModel;
 
 public partial class SkinsViewModel : ObservableObject
 {
+    
     [ObservableProperty]
-    bool isAddEnable = true;
+    string url;
+    [ObservableProperty]
+    string championName;
+    [ObservableProperty]
+    string skinName;
+    [ObservableProperty]
+    int skinPrice;
+    [ObservableProperty]
+    bool isAddButtonEnabled = true;
+    [ObservableProperty]
+
 
     public SkinsService skinsService;
     public ObservableCollection<Skins> mySkinList { get; } = new();
@@ -12,6 +26,52 @@ public partial class SkinsViewModel : ObservableObject
     public SkinsViewModel(SkinsService mySkins)
     {
         this.skinsService = mySkins;
+    }
+
+    public void GetNewSkin()
+    {
+        // Access entry values and use them as needed
+        string url = Url;
+        string championName = ChampionName;
+        string skinName = SkinName;
+        int price = SkinPrice;
+    }
+
+    public void CreateNewSkin()
+    {
+        Skins newSkin = new Skins
+        {
+            Image = Url,
+            ChampionName = ChampionName,
+            Name = SkinName,
+            Price = SkinPrice
+        };
+
+        mySkinList.Add(newSkin);
+    }
+
+    [RelayCommand]
+    public void CheckEntriesValue()
+    {
+        bool allEntriesValid = !string.IsNullOrEmpty(Url) &&
+                               !string.IsNullOrEmpty(ChampionName) &&
+                               !string.IsNullOrEmpty(SkinName) &&
+                               SkinPrice > 0;
+
+        if (!allEntriesValid)
+        {
+            DisableAddButton();
+        }
+
+        IsAddButtonEnabled = allEntriesValid;
+        OnPropertyChanged(nameof(IsAddButtonEnabled));
+        GetNewSkin();
+    }
+
+    private void DisableAddButton()
+    {
+        IsAddButtonEnabled = false;
+        OnPropertyChanged(nameof(IsAddButtonEnabled));
     }
 
     [RelayCommand]
@@ -27,6 +87,31 @@ public partial class SkinsViewModel : ObservableObject
             await Shell.Current.DisplayAlert("Error : ", ex.Message, "OK");
         }
 
+    }
+
+    private async Task AddSkinToJson(Skins newSkin)
+    {
+        try
+        {
+            // Read the existing JSON file into a string
+            string jsonString = await File.ReadAllTextAsync("skinsdata.json");
+
+            // Deserialize the JSON string into a list of objects
+            List<Skins> skinsList = JsonConvert.DeserializeObject<List<Skins>>(jsonString);
+
+            // Add the new object to the list
+            skinsList.Add(newSkin);
+
+            // Serialize the updated list back into a JSON string
+            string updatedJsonString = JsonConvert.SerializeObject(skinsList, Formatting.Indented);
+
+            // Write the JSON string back to the file
+            await File.WriteAllTextAsync("skinsdata.json", updatedJsonString);
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error : ", ex.Message, "OK");
+        }
     }
 
 }
