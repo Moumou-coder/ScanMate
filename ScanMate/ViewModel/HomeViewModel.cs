32f40955c2e25fb4ef6b6c8f73af4fe7f2c18447
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-05-17 15:48:57 +0200

    put all logic of mainPage into HomePage

--- ScanMate/ViewModel/HomeViewModel.cs
+++ ScanMate/ViewModel/HomeViewModel.cs
@@ -1,9 +1,36 @@
 namespace ScanMate.ViewModel;
 
-public class HomeViewModel : ContentPage
+public partial class HomeViewModel : ObservableObject
 {
-	public HomeViewModel()
-	{
+    BarCodeService MyBarCodeService;
 
-	}
+    [ObservableProperty]
+    string myTitle = "League Of Legend V1.0";
+    [ObservableProperty]
+    string myID = "Mister M";
+
+    public HomeViewModel()
+	{
+        this.MyBarCodeService = new BarCodeService();
+        MyBarCodeService.ConfigureScanner();
+        MyBarCodeService.SerialBuffer.Changed += SerialBuffer_Changed;
+    }
+
+    private void SerialBuffer_Changed(object sender, EventArgs e)
+    {
+        BarCodeService.QueueBuffer myQueue = (BarCodeService.QueueBuffer)sender;
+        MyID = myQueue.Dequeue().ToString(); //ActiveTarget = nom du label a changer!!!!
+    }
+
+    [RelayCommand]
+    public async Task GoToChampionsPage()
+    {
+        await Shell.Current.GoToAsync(nameof(ChampionsPage));
+    }
+
+    [RelayCommand]
+    public async Task GoToSkinsPage()
+    {
+        await Shell.Current.GoToAsync(nameof(SkinsPage));
+    }
 }
--- ScanMate/ViewModel/MainViewModel.cs
+++ ScanMate/ViewModel/MainViewModel.cs
@@ -2,36 +2,9 @@ namespace ScanMate.ViewModel;
 
 public partial class MainViewModel : ObservableObject
 {
-    BarCodeService MyBarCodeService;
-
-    [ObservableProperty]
-    string myTitle = "League Of Legend V1.0";
-    [ObservableProperty]
-    string myID ="Mister M";
-
     public MainViewModel()
     {
-        this.MyBarCodeService = new BarCodeService();
-        MyBarCodeService.ConfigureScanner();
-        MyBarCodeService.SerialBuffer.Changed += SerialBuffer_Changed;
-    }
 
-    private void SerialBuffer_Changed(object sender, EventArgs e)
-    {
-        BarCodeService.QueueBuffer myQueue = (BarCodeService.QueueBuffer)sender;
-        MyID = myQueue.Dequeue().ToString(); //ActiveTarget = nom du label a changer!!!!
-    }
-
-    [RelayCommand]
-    public async Task GoToChampionsPage()
-    {
-        await Shell.Current.GoToAsync(nameof(ChampionsPage));
-    }
-    
-    [RelayCommand]
-    public async Task GoToSkinsPage()
-    {
-            await Shell.Current.GoToAsync(nameof(SkinsPage));
     }
 
     [RelayCommand]
