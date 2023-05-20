8506b1d9b551ac3d31d805877bc3d0d22d230004
Moumou-coder <57679398+Moumou-coder@users.noreply.github.com>
2023-05-20 19:08:33 +0200

    add user model and championsdata

--- ScanMate/Services/ChampionsService.cs
+++ ScanMate/Services/ChampionsService.cs
@@ -5,14 +5,21 @@ public partial class ChampionsService : ContentPage
 	public ChampionsService() { }
 
     List<Champions> championsList;
+
     public async Task<List<Champions>> GetChampions()
     {
 
-        using var stream = await FileSystem.OpenAppPackageFileAsync("ChampionsData.json");
-        using var reader = new StreamReader(stream);
-        var contents = await reader.ReadToEndAsync();
-        championsList = JsonSerializer.Deserialize<List<Champions>>(contents);
+        string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ScanMateServer", "championsdata.json");
+        string jsonContent;
 
+        // Read the file contents
+        using var stream = new FileStream(filePath, FileMode.Open);
+        using var reader = new StreamReader(stream);
+        jsonContent = await reader.ReadToEndAsync();
+        // Deserialize the JSON into a list of skins
+        championsList = JsonSerializer.Deserialize<List<Champions>>(jsonContent);
+
+        // Return the filled list
         return championsList;
     }
 }
